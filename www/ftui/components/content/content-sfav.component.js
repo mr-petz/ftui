3af7c1d1c8f243409259f4f6c6d57174f5d3dd6d
mr-petz <35274137+mr-petz@users.noreply.github.com>
2022-03-12 17:45:33 +0100

    Add files via upload

---
+++ www/ftui/components/content/content-sfav.component.js
@@ -0,0 +1,135 @@
+/*
+* SonosFavRG component for FTUI version 3
+*
+* by mr_petz
+*
+* Under MIT License (http://www.opensource.org/licenses/mit-license.php)
+*
+* https://github.com/knowthelist/ftui
+*/
+
+import { FtuiContent } from './content.component.js';
+
+export class FtuiContentSonosFavRG extends FtuiContent {
+
+  constructor(properties) {
+    super(Object.assign(FtuiContentSonosFavRG.properties, properties));
+    this.drop = this.shadowRoot.querySelector('.label');
+    this.dropList = this.shadowRoot.querySelector('.list');
+    this.drop.addEventListener('click', () => this.showList());
+    this.dropList.addEventListener('click', () => this.showList());
+  }
+
+  template() {
+    return `
+      <style>
+      :host {
+        width: 100%;
+      }
+
+      .label {
+        cursor: pointer;
+        margin-left: 2px;
+        color: var(--text-color);
+      }
+
+      .drop {
+        position: absolute;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        width: 90%;
+        left: 5%;
+        height: 160px;
+      }
+
+      .drop .list {
+        position: absolute;
+        visibility: hidden;
+        width: 100%;
+        height: 100%;
+        background-color: var(--grid-background-color);
+        text-align: center;
+        border-radius: 5px;
+        left: 0;
+        top: 5%;
+        overflow: scroll;
+        overflow-y: auto;
+        overflow-x: hidden;
+        scrollbar-width: none;
+        -ms-overflow-style: none;
+        z-index: 9999;
+      }
+
+      .drop .show {
+        visibility: visible;
+        cursor: pointer;
+        -webkit-animation: fadeIn 1s;
+        animation: fadeIn 1s;
+      }
+
+      .caret {
+        color: var(--text-color);
+        display: inline-block;
+        margin-left: 0;
+        width: 0;
+        height: 0;
+        vertical-align: middle;
+        border-top: 4px dashed;
+        border-top: 4px solid;
+        border-right: 4px solid transparent;
+        border-left: 4px solid transparent;
+        cursor: pointer;
+      }
+
+      @-webkit-keyframes fadeIn {
+        from {opacity: 0;}
+        to {opacity: 1;}
+      }
+
+      @keyframes fadeIn {
+        from {opacity: 0;}
+        to {opacity:1 ;}
+      }
+      </style>
+      <div class="label">${this.label}
+        <span class="caret"></span>
+      </div>
+      <div class="drop">
+        <span class="list"></span>
+      </div>
+    `;
+  }
+
+  static get properties() {
+    return {
+      label: 'FavSonos',
+      content: '',
+    };
+  }
+
+  static get observedAttributes() {
+    return [...this.convertToAttributes(FtuiContentSonosFavRG.properties), ...super.observedAttributes];
+  }
+
+  onAttributeChanged(name) {
+    switch (name) {
+      case 'content':
+        const deviceName = this.binding.getReadingsOfAttribute('content')[0];
+        const regexp = /informid\=\"(\w*)\-item\:1\:1"/gi;
+        const rgName = regexp.exec(this.content);
+        const replaceString ='/fhem?XHR=1&cmd.'+deviceName+'%3D';
+        this.dropList.innerHTML = this.content.replaceAll(replaceString,'').replace(/%20/g,' ').replace(/%2520/g,'%20').replace(/%2528/g,'%28').replace(/%2529/g,'%29').replace(/FW_cmd/g,'ftuiApp.fhemService.sendCommand');
+        const view = 'div[informid="'+rgName[1]+'-item:1:1"]';
+        this.dropList.innerHTML=this.dropList.querySelector(view).innerHTML;
+      break;
+    }
+  }
+
+  showList() {
+    this.dropList.classList.toggle('show');
+  }
+}
+
+window.customElements.define('ftui-content-sfav', FtuiContentSonosFavRG);
