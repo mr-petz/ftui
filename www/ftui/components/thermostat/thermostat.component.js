a34af82403b507ce5fd22f617774920f822681f2
mr-petz <35274137+mr-petz@users.noreply.github.com>
2023-01-24 19:55:06 +0100

    battery blink fixed

--- www/ftui/components/thermostat/thermostat.component.js
+++ www/ftui/components/thermostat/thermostat.component.js
@@ -639,7 +639,7 @@ export class FtuiThermostat extends FtuiElement {
   }
 
   batteryValue() {
-    (this.battery<25?this.battIcon.classList.add('blink'):this.battIcon.classList.remove('blink'));
+    (this.battery.replace(/[^\d.-]/g, '')<25?this.battIcon.classList.add('blink'):this.battIcon.classList.remove('blink'));
     this.batt.textContent = this.battery;
   }
  
